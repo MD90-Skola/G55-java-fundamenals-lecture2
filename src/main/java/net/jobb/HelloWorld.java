c2b796c6c949a117c6239ae0b62d53a87dae5d5d
MD90-Skola <micheldahl90@outlook.com>
2025-03-11 21:31:25 +0100

    lite update

--- src/main/java/net/jobb/HelloWorld.java
+++ src/main/java/net/jobb/HelloWorld.java
@@ -11,18 +11,42 @@ public class HelloWorld {
 
 
     /**
-     *
-     *
-     *    kan skriva java kod utan den körs  </B> taggar osv
-     *
-     *
+     * kan skriva java kod utan den körs  </B> taggar osv
      */
 
     // psvm + tab
     public static void main(String[] args) {
+        System.out.println("tja");
+
+        int speed;
+        speed = 100;
+
+        double salary = 50000.55;
+        char grade = 'a';
+        char specialChar = '\u2764';   // <3
+        boolean isActive = true;
+        boolean isReserved = true;
+
+        long balance = 5000000000L;
+        double area = 20.55;
+        int setNumber = 100;
+        int studentId = 5000;
+
+        System.out.println("speed: " + speed);
+        System.out.println("-----------------------");
+
+        int number1 = 10;
+        int number2 = 5;
+
+        int additionResult = number1 + number2;
+        System.out.println("addition: " + additionResult);
+
+        // *    /%
+
+        // divison vs modules
+        int division = number1 / number2;
+        System.out.println(division);
 
 
-        // sout + tab
-        System.out.println("hej");}
     }
-
+}
