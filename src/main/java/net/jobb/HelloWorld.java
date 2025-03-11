4de1090c818c5b41df9af27378b8f53e9ca65167
MD90-Skola <micheldahl90@outlook.com>
2025-03-11 19:26:04 +0100

    tst

---
+++ src/main/java/net/jobb/HelloWorld.java
@@ -0,0 +1,28 @@
+package net.jobb;
+
+public class HelloWorld {
+
+
+    // = singel line comment
+
+    /*
+    Multi line comment
+     */
+
+
+    /**
+     *
+     *
+     *    kan skriva java kod utan den körs  </B> taggar osv
+     *
+     *
+     */
+
+    // psvm + tab
+    public static void main(String[] args) {
+
+
+        // sout + tab
+        System.out.println("hej");}
+    }
+
